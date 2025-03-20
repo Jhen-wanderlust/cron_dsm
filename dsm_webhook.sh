e3230e81e42709a9d812ca704f4b7dc0014452a3
Jhen-wanderlust <56061703+Jhen-wanderlust@users.noreply.github.com>
2025-03-21 01:53:19 +0800

    Add files via upload

---
+++ dsm_webhook.sh
@@ -0,0 +1,3 @@
+curl -X POST -H 'Content-type: application/json' --data @timeZoneData.json https://hooks.slack.com/services/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
+
+echo "Payload sent to Slack webhook successfully."
