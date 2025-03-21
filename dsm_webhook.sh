bb2f994f689cab5f3528f7f99fccdb158d0d040e
Jhen-wanderlust <56061703+Jhen-wanderlust@users.noreply.github.com>
2025-03-21 14:02:53 +0800

    Update dsm_webhook.sh

--- dsm_webhook.sh
+++ dsm_webhook.sh
@@ -1,3 +1,3 @@
-curl -X POST -H 'Content-type: application/json' --data @timeZoneData.json https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data @timeZoneData.json https://hooks.slack.com/services/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
 
 echo "Payload sent to Slack webhook successfully."
